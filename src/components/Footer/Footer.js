60fbcddd850beb838556abb0cd4e2edc2cb9dd07
eastend-street <ss_1322irf@icloud.com>
2019-06-01 17:16:06 -0700

    add temporary social logo in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import githubLogo from "../../static/images/logo/github.png";
+import linkedInLogo from "../../static/images/logo/linkedIn.png";
+import FacebookLogo from "../../static/images/logo/facebook.png";
 
 const StyledFooter = styled.div`
   /* background-color: #d7ccc8; */
@@ -8,16 +11,52 @@ const StyledFooter = styled.div`
 
 const Copyright = styled.p`
   text-align: center;
-  color: #777;
+  color: #aea9a5;
+  margin-top: 5rem;
+`;
+
+const ContentSocial = styled.div`
+  text-align: center;
+  margin-top: 2rem;
+`;
+
+const SocialLogo = styled.img`
+  text-align: center;
+  height: 2rem;
+  margin: auto 1rem;
+  :hover {
+    opacity: 0.5;
+  }
 `;
 
 class Footer extends Component {
   render() {
     return (
-      <StyledFooter className="footer">
-        <Copyright>
-          &copy; Jun Yamada 2018-2019 All rights reserved.
-        </Copyright>
+      <StyledFooter>
+        <ContentSocial>
+          <a
+            href="https://github.com/eastend-street"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SocialLogo src={githubLogo} alt="Github" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/jun-yamada-0097"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SocialLogo src={linkedInLogo} alt="LinkedIn" />
+          </a>
+          <a
+            href="https://www.facebook.com/yamada.junya1211"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SocialLogo src={FacebookLogo} alt="Facebook" />
+          </a>
+        </ContentSocial>
+        <Copyright>&copy; Jun Yamada 2018-2019 All rights reserved.</Copyright>
       </StyledFooter>
     );
   }
