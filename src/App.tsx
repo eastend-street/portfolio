bfca7b13f2ca6372493f96190dd1849a7a14c764
eastend-street <ss_1322irf@icloud.com>
2020-03-01 01:49:26 -0800

    remove open sans

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ import Brickhouse from "./components/Brickhouse/Brickhouse";
 
 const GlobalStyle = createGlobalStyle`
   body {
-    /* @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap'); */
+    @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
     margin: 0;
     padding: 0;
     /* font-family: 'Open Sans', sans-serif; */
