cf9e5b7414088d3253c5a598d143aa9840914854
eastend-street <ss_1322irf@icloud.com>
2020-03-23 00:42:25 -0700

    remove open sans

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,11 @@ import Brickhouse from "./components/Brickhouse/Brickhouse";
 
 const GlobalStyle = createGlobalStyle`
   body {
-    @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
+    /* @import url('//fonts.googleapis.com/css?family=Open+Sans&display=swap'); */
     margin: 0;
     padding: 0;
     font-family: 'Open Sans', sans-serif;
+    font-family: sans-serif;
     color: #000;
     background-color: #e3bda3;
     background-color: #EEE2D7;
--- src/theme/GlobalStyle.ts
+++ src/theme/GlobalStyle.ts
@@ -2,10 +2,10 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
-    @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
+    /* @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap'); */
     margin: 0;
     padding: 0;
-    font-family: 'Open Sans', ssans-serif;
+    font-family: 'Open Sans', sans-serif;
     color: #000;
     background-color: #EEE2D7;
   }
