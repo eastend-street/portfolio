7467d6c1f879a7d8bab8db28df6bb0bd25c2ed08
eastend-street <suminari.yamada@gmail.com>
2020-05-21 10:11:14 -0700

    clean up not fonund page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { FC } from "react";
 import { Helmet } from "react-helmet";
 import styled from "styled-components";
 
 import { Grid } from "@material-ui/core";
 import StyledButton from "components/ChildComponents/StyledButton/StyledButton";
+
+const NotFound: FC = () => (
+  <Container container>
+    <Helmet>
+      <title>Page not found</title>
+    </Helmet>
+    <Grid item xs={11} sm={12}>
+      <StyledH1>404 Page not found</StyledH1>
+      <StyledH3>
+        Sorry, I couldn't find the page...
+        <br />
+        Please click the button below to go back to the Homepage.
+      </StyledH3>
+      <ContentButton>
+        <StyledButton name="Go to Homepage" url="/" target="_self" />
+      </ContentButton>
+    </Grid>
+  </Container>
+);
 
 const Container = styled(Grid)`
   && {
@@ -25,26 +44,5 @@ const ContentButton = styled.div`
   margin: 2rem;
   text-align: center;
 `;
-
-const NotFound: React.FC = () => {
-  return (
-    <Container container>
-      <Helmet>
-        <title>Page not found</title>
-      </Helmet>
-      <Grid item xs={11} sm={12}>
-        <StyledH1>404 Page not found</StyledH1>
-        <StyledH3>
-          Sorry, I couldn't find the page...
-          <br />
-          Please click the button below to go back to the Homepage.
-        </StyledH3>
-        <ContentButton>
-          <StyledButton name="Go to Homepage" url="/" target="_self" />
-        </ContentButton>
-      </Grid>
-    </Container>
-  );
-};
 
 export default NotFound;
