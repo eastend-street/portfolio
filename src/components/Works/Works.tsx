c6fb245fb360b1b8fcf0b7f3e1c8e407985e06af
eastend-street <suminari.yamada@gmail.com>
2020-06-03 20:11:45 -0700

    moved styled-components, change class to functional components

--- src/components/Works/Works.tsx
+++ src/components/Works/Works.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { FC } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
@@ -9,6 +9,171 @@ import CodeoPhoto from "assets/images/websites/codeo/codeo.jpg";
 import MarketplacePhoto from "assets/images/websites/marketplace/marketplace-logo.png";
 import SlackBotPhoto from "assets/images/websites/slack-bot/slack-bot-logo.png";
 import BrickHousePhoto from "assets/images/websites/brickhouse/brickhouse.jpg";
+
+const Works: FC = () => {
+  const renderWorkCard = () => (
+    <>
+      <GridCardItem item xs={12} md={12}>
+        <Grid container>
+          <JotDownImageGrid item xs={12} md={6}>
+            <WorkCard
+              url={process.env.PUBLIC_URL + "/jotdown"}
+              image={JotDownPhoto}
+            />
+          </JotDownImageGrid>
+          <JotDownTitleGrid item xs={12} md={6}>
+            <div>
+              <StyledLink to={process.env.PUBLIC_URL + "/jotdown"}>
+                <ServiceName variant="h5" component="h3">
+                  Jot down
+                </ServiceName>
+              </StyledLink>
+              <ServiceDescription variant="subtitle1" component="p">
+                - Bookmark and note web service -
+              </ServiceDescription>
+              <Technologies>
+                <TechName>React.js</TechName>
+                <TechName>Redux</TechName>
+                <TechName>Django</TechName>
+                <TechName>Python</TechName>
+                <TechName>Docker</TechName>
+              </Technologies>
+            </div>
+          </JotDownTitleGrid>
+        </Grid>
+      </GridCardItem>
+      <GridCardItem item xs={12} md={12}>
+        <Grid container>
+          <CodeoTitleGrid item xs={12} md={6}>
+            <div>
+              <AboutService>
+                <StyledLink to={process.env.PUBLIC_URL + "/codeo"}>
+                  <ServiceName variant="h5" component="h3">
+                    Codeo
+                  </ServiceName>
+                </StyledLink>
+                <ServiceDescription variant="subtitle1" component="p">
+                  - Search programming video from YouTube -
+                </ServiceDescription>
+                <Technologies>
+                  <TechName>React.js</TechName>
+                  <TechName>React hooks</TechName>
+                  <TechName>Context API</TechName>
+                  <TechName>TypeScript</TechName>
+                </Technologies>
+              </AboutService>
+            </div>
+          </CodeoTitleGrid>
+          <Grid item xs={12} md={6}>
+            <WorkCard
+              url={process.env.PUBLIC_URL + "/codeo"}
+              image={CodeoPhoto}
+            />
+          </Grid>
+        </Grid>
+      </GridCardItem>
+      <GridCardItem item xs={12} md={12}>
+        <Grid container>
+          <BrickhouseImageGrid item xs={12} md={6}>
+            <WorkCard
+              url={process.env.PUBLIC_URL + "/brickhouse"}
+              image={BrickHousePhoto}
+            />
+          </BrickhouseImageGrid>
+          <BrickhouseTitleGrid item xs={12} md={6}>
+            <div>
+              <StyledLink to={process.env.PUBLIC_URL + "/brickhouse"}>
+                <ServiceName variant="h5" component="h3">
+                  Brickhouse Ventures
+                </ServiceName>
+              </StyledLink>
+              <ServiceDescription variant="subtitle1" component="p">
+                - Brickhouse company homepage -
+              </ServiceDescription>
+              <Technologies>
+                <TechName>React.js</TechName>
+                <TechName>React hooks</TechName>
+                <TechName>TypeScript</TechName>
+              </Technologies>
+            </div>
+          </BrickhouseTitleGrid>
+        </Grid>
+      </GridCardItem>
+      <GridCardItem item xs={12} md={12}>
+        <Grid container>
+          <MarketplaceTitleGrid item xs={12} md={6}>
+            <div>
+              <AboutService>
+                <StyledLink to={process.env.PUBLIC_URL + "/marketplace"}>
+                  <ServiceName variant="h5" component="h3">
+                    Mail Marketplace
+                  </ServiceName>
+                </StyledLink>
+                <ServiceDescription variant="subtitle1" component="p">
+                  - Mail advertisement trading system -
+                </ServiceDescription>
+                <Technologies>
+                  <TechName>Vue.js</TechName>
+                  <TechName>Vuex</TechName>
+                  <TechName>Django</TechName>
+                  <TechName>Python</TechName>
+                  <TechName>Docker</TechName>
+                  <TechName>Google Cloud Platform</TechName>
+                </Technologies>
+              </AboutService>
+            </div>
+          </MarketplaceTitleGrid>
+          <Grid item xs={12} md={6}>
+            <WorkCard
+              url={process.env.PUBLIC_URL + "/marketplace"}
+              image={MarketplacePhoto}
+            />
+          </Grid>
+        </Grid>
+      </GridCardItem>
+      <GridCardItem item xs={12} md={12}>
+        <Grid container>
+          <SlackBotImageGrid item xs={12} md={6}>
+            <WorkCard
+              url={process.env.PUBLIC_URL + "/slack-bot"}
+              image={SlackBotPhoto}
+            />
+          </SlackBotImageGrid>
+          <SlackBotTitleGrid item xs={12} md={6}>
+            <div>
+              <StyledLink to={process.env.PUBLIC_URL + "/slack-bot"}>
+                <ServiceName variant="h5" component="h3">
+                  Translation Slack bot
+                </ServiceName>
+              </StyledLink>
+              <ServiceDescription variant="subtitle1" component="p">
+                - Multilingual translation Slack bot -
+              </ServiceDescription>
+              <Technologies>
+                <TechName>Python</TechName>
+                <TechName>Docker</TechName>
+                <TechName>Google Translation API</TechName>
+              </Technologies>
+            </div>
+          </SlackBotTitleGrid>
+        </Grid>
+      </GridCardItem>
+    </>
+  );
+
+  return (
+    <ContentWorks container justify="center" id="works">
+      <Grid item xs={12}>
+        <Title variant="h4" component="h2">
+          WORKS
+        </Title>
+      </Grid>
+      <Grid item xs={11} md={10}>
+        <Grid container>{renderWorkCard}</Grid>
+      </Grid>
+    </ContentWorks>
+  );
+};
 
 const ContentWorks = styled(Grid)`
   && {
@@ -177,174 +342,5 @@ const StyledLink = styled(Link)`
     text-decoration: none;
   }
 `;
-
-class Works extends Component {
-  renderWorkCard() {
-    return (
-      <React.Fragment>
-        <GridCardItem item xs={12} md={12}>
-          <Grid container>
-            <JotDownImageGrid item xs={12} md={6}>
-              <WorkCard
-                url={process.env.PUBLIC_URL + "/jotdown"}
-                image={JotDownPhoto}
-              />
-            </JotDownImageGrid>
-            <JotDownTitleGrid item xs={12} md={6}>
-              <div>
-                <StyledLink to={process.env.PUBLIC_URL + "/jotdown"}>
-                  <ServiceName variant="h5" component="h3">
-                    Jot down
-                  </ServiceName>
-                </StyledLink>
-                <ServiceDescription variant="subtitle1" component="p">
-                  - Bookmark and note web service -
-                </ServiceDescription>
-                <Technologies>
-                  <TechName>React.js</TechName>
-                  <TechName>Redux</TechName>
-                  <TechName>Django</TechName>
-                  <TechName>Python</TechName>
-                  <TechName>Docker</TechName>
-                </Technologies>
-              </div>
-            </JotDownTitleGrid>
-          </Grid>
-        </GridCardItem>
-        <GridCardItem item xs={12} md={12}>
-          <Grid container>
-            <CodeoTitleGrid item xs={12} md={6}>
-              <div>
-                <AboutService>
-                  <StyledLink to={process.env.PUBLIC_URL + "/codeo"}>
-                    <ServiceName variant="h5" component="h3">
-                      Codeo
-                    </ServiceName>
-                  </StyledLink>
-                  <ServiceDescription variant="subtitle1" component="p">
-                    - Search programming video from YouTube -
-                  </ServiceDescription>
-                  <Technologies>
-                    <TechName>React.js</TechName>
-                    <TechName>React hooks</TechName>
-                    <TechName>Context API</TechName>
-                    <TechName>TypeScript</TechName>
-                  </Technologies>
-                </AboutService>
-              </div>
-            </CodeoTitleGrid>
-            <Grid item xs={12} md={6}>
-              <WorkCard
-                url={process.env.PUBLIC_URL + "/codeo"}
-                image={CodeoPhoto}
-              />
-            </Grid>
-          </Grid>
-        </GridCardItem>
-        <GridCardItem item xs={12} md={12}>
-          <Grid container>
-            <BrickhouseImageGrid item xs={12} md={6}>
-              <WorkCard
-                url={process.env.PUBLIC_URL + "/brickhouse"}
-                image={BrickHousePhoto}
-              />
-            </BrickhouseImageGrid>
-            <BrickhouseTitleGrid item xs={12} md={6}>
-              <div>
-                <StyledLink to={process.env.PUBLIC_URL + "/brickhouse"}>
-                  <ServiceName variant="h5" component="h3">
-                    Brickhouse Ventures
-                  </ServiceName>
-                </StyledLink>
-                <ServiceDescription variant="subtitle1" component="p">
-                  - Brickhouse company homepage -
-                </ServiceDescription>
-                <Technologies>
-                  <TechName>React.js</TechName>
-                  <TechName>React hooks</TechName>
-                  <TechName>TypeScript</TechName>
-                </Technologies>
-              </div>
-            </BrickhouseTitleGrid>
-          </Grid>
-        </GridCardItem>
-        <GridCardItem item xs={12} md={12}>
-          <Grid container>
-            <MarketplaceTitleGrid item xs={12} md={6}>
-              <div>
-                <AboutService>
-                  <StyledLink to={process.env.PUBLIC_URL + "/marketplace"}>
-                    <ServiceName variant="h5" component="h3">
-                      Mail Marketplace
-                    </ServiceName>
-                  </StyledLink>
-                  <ServiceDescription variant="subtitle1" component="p">
-                    - Mail advertisement trading system -
-                  </ServiceDescription>
-                  <Technologies>
-                    <TechName>Vue.js</TechName>
-                    <TechName>Vuex</TechName>
-                    <TechName>Django</TechName>
-                    <TechName>Python</TechName>
-                    <TechName>Docker</TechName>
-                    <TechName>Google Cloud Platform</TechName>
-                  </Technologies>
-                </AboutService>
-              </div>
-            </MarketplaceTitleGrid>
-            <Grid item xs={12} md={6}>
-              <WorkCard
-                url={process.env.PUBLIC_URL + "/marketplace"}
-                image={MarketplacePhoto}
-              />
-            </Grid>
-          </Grid>
-        </GridCardItem>
-        <GridCardItem item xs={12} md={12}>
-          <Grid container>
-            <SlackBotImageGrid item xs={12} md={6}>
-              <WorkCard
-                url={process.env.PUBLIC_URL + "/slack-bot"}
-                image={SlackBotPhoto}
-              />
-            </SlackBotImageGrid>
-            <SlackBotTitleGrid item xs={12} md={6}>
-              <div>
-                <StyledLink to={process.env.PUBLIC_URL + "/slack-bot"}>
-                  <ServiceName variant="h5" component="h3">
-                    Translation Slack bot
-                  </ServiceName>
-                </StyledLink>
-                <ServiceDescription variant="subtitle1" component="p">
-                  - Multilingual translation Slack bot -
-                </ServiceDescription>
-                <Technologies>
-                  <TechName>Python</TechName>
-                  <TechName>Docker</TechName>
-                  <TechName>Google Translation API</TechName>
-                </Technologies>
-              </div>
-            </SlackBotTitleGrid>
-          </Grid>
-        </GridCardItem>
-      </React.Fragment>
-    );
-  }
-
-  render() {
-    return (
-      <ContentWorks container justify="center" id="works">
-        <Grid item xs={12}>
-          <Title variant="h4" component="h2">
-            WORKS
-          </Title>
-        </Grid>
-        <Grid item xs={11} md={10}>
-          <Grid container>{this.renderWorkCard()}</Grid>
-        </Grid>
-      </ContentWorks>
-    );
-  }
-}
 
 export default Works;
