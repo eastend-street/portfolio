b4c9200d9e457ddae6f21f6febafde9de50d235e
eastend-street <suminari.yamada@gmail.com>
2020-08-30 19:59:02 -0700

    removed global smooth scroll

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -1,9 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  html {
-    scroll-behavior: smooth;
-  }
   body {
     margin: 0;
     padding: 0;
