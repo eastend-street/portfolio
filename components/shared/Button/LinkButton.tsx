fb5ec58f20e38ad163be3a874d74a29afe526f4c
eastend-street <suminari.yamada@gmail.com>
2022-02-06 16:54:21 -0800

    Remove prefetch which is no longer required after Next.js update

--- components/shared/Button/LinkButton.tsx
+++ components/shared/Button/LinkButton.tsx
@@ -6,16 +6,14 @@ interface LinkButtonProps extends LinkProps {
   name: string;
   href: string;
   target?: string;
-  prefetch?: boolean
 }
 
 const LinkButton: React.FC<LinkButtonProps> = ({
   name,
   href,
   target = '_self',
-  prefetch = true,
 }) => (
-  <Link href={href} prefetch={prefetch}>
+  <Link href={href}>
     {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
     <a target={target} data-testid={`link-${name}`}>
       <Button>{name}</Button>
