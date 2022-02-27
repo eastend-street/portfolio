8c65c9669a3a7e06ee14f97f10e19b3220c982be
eastend-street <suminari.yamada@gmail.com>
2022-02-26 19:45:05 -0800

    Resolve absolute path(Root directory) & assets files under public

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
@@ -5,5 +7,10 @@ module.exports = {
   core: {
     builder: 'webpack5',
   },
+  staticDirs: ['../public'],
   typescript: { reactDocgen: false },
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules'];
+    return config;
+  },
 };
