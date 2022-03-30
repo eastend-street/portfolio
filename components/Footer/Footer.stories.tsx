51af002aec4b092e8b339aae77119f4098dde624
eastend-street <suminari.yamada@gmail.com>
2022-03-29 22:29:25 -0700

    Add Footer storybook component

---
+++ components/Footer/Footer.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Story, ComponentMeta } from '@storybook/react';
+
+import Footer from './Footer';
+
+export default {
+  title: 'Components/Footer',
+  component: Footer,
+} as ComponentMeta<typeof Footer>;
+
+export const Default = {};
