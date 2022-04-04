2472ec1f15299f890ce46741ccd0edeefc2da341
eastend-street <suminari.yamada@gmail.com>
2022-04-03 17:43:46 -0700

    Add target blank as a default value for ExternalLink

--- components/shared/ExternalLink/ExternalLink.tsx
+++ components/shared/ExternalLink/ExternalLink.tsx
@@ -3,10 +3,11 @@ import { FC } from 'react';
 import { ExternalLinkProps } from './types';
 
 const ExternalLink: FC<ExternalLinkProps> = ({
+  target = "_blank",
   children,
   ...restProps
 }) => (
-  <a {...restProps}>
+  <a  target={target} {...restProps}>
     {children}
   </a>
 );
